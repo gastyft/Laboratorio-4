12f5544486e39eed70f526c12edb694725118567
gastyft <gastyftacla1@hotmail.com>
2024-09-09 17:19:46 -0300

    finalizacion parte 1

--- TP 1/forms.js
+++ TP 1/forms.js
@@ -1,23 +1,34 @@
 
 class person{ 
-  constructor(nombre,apellido, edad, email, contraseña,fechaNac,dni){
+  constructor(nombre,apellido, edad,dni, email, contraseña,fechaNac){
 
 
 this.nombre=nombre;
 this.apellido=apellido;
 this.contraseña=contraseña;
 this.email=email;
+this.dni=dni;
 this.edad=edad;
 this.fechaNac=fechaNac;
-this.dni=dni;
+
+
 }
 }
 
 let arrayStd=[];
 
-
+function capitalizeFirstLetter(str) { //Con ayudin
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+function capitalizeWords(str) { //Con ayudin
+  return str
+      .split(' ')           // Divide la cadena en un array por espacios
+      .map(word => capitalizeFirstLetter(word)) // Capitaliza cada palabra
+      .join(' ');           // Une las palabras de nuevo en una sola cadena
+}
 
 function validarString(str) {
+  
   // Expresión para verificar si el string contiene solo letras, espacios y acentos
   const pattern = /^[a-zA-Z\u00C0-\u017F\s]*$/;
   if (str.length > 20) {
@@ -121,13 +132,16 @@ function validarContraseña(password) {
 }
 
 function chequear() {
-    const nombre = document.getElementById("nombre").value.trim();
-    const apellido = document.getElementById("apellido").value.trim();
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value ;
     const contraseña = document.getElementById("contraseña").value.trim();
     const email = document.getElementById("email").value.trim();
     const fechaNac = document.getElementById("fechaNac").value;
     const dni = document.getElementById("dni").value;
   
+    nombre = capitalizeWords(nombre);
+    apellido = capitalizeWords(apellido);
+
   if (!validarString(nombre) || nombre.length == 0) {
     mostrarAlertaError("Nombre");
     return;
@@ -152,10 +166,7 @@ function chequear() {
     mostrarAlertaError("Email");
     return;
   }
-  if(!validarFecha(fechaNac)){
-    mostrarAlertaError("Fecha de nacimiento");
-    return;
-  }
+
   if(!validarDNI(dni)){
     mostrarAlertaError("DNI");
     return;
@@ -165,13 +176,14 @@ function chequear() {
   return;
   }
   //agregar a array y calcula edad 
-  agregarArray(new person(nombre,apellido,calcularEdad(fechaNac),email,contraseña,fechaNac));
+  agregarArray(new person(nombre,apellido,calcularEdad(fechaNac),dni,email,contraseña,fechaNac));
   //limpiar los campos
   document.getElementById("nombre").value = "";
   document.getElementById("apellido").value = "";
   document.getElementById("contraseña").value = "";
   document.getElementById("email").value = "";
   document.getElementById("fechaNac").value = "";
+  document.getElementById("dni").value = "";
   //mostrar alerta de exito
   //agregar a array
   //limpiar los campos
@@ -188,6 +200,7 @@ const estudiantesDiv = document.getElementById("estudiantes");
 estudiantesDiv.style.display = "block";
 estudiantesDiv.removeAttribute("hidden"); // Elimina el atributo hidden
 }
+
 function volverForm(){
   document.querySelector(".form-horizontal").style.display="block";   
   const estudiantesDiv = document.getElementById("estudiantes");
@@ -215,31 +228,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-let productos = [];
 
-const producto1 = "Primer producto";
-const producto2 = "Segundo producto";
-const producto3 = "Tercer producto";
-const producto4 = "Cuarto producto";
-
-
- 
- 
-  function agregarProducto(producto) {
-    if (!productos.includes(producto)) {
-        productos.push(producto);
-        console.log(productos);
-        renderizarListaDeseados();
-    } else {
-      swal("Ups!", "Este producto ya está en la lista de deseados.", "warning");
-        
-    }
+function ordenarAlfabetic(){
+  arrayStd.sort((a, b) => a.apellido.localeCompare(b.apellido));
 }
 function renderizarListaEstudiantes() {
   const listaElement = document.getElementById('estudiantes-list');
   listaElement.innerHTML = ''; // Limpia la lista antes de renderizar
   let i = 1;
-
+  //function para ordenar la lista alfabeticamente
+  ordenarAlfabetic();
   arrayStd.forEach((_estudiante) => {
     const fila = document.createElement('tr'); // Crea una nueva fila
             
@@ -253,44 +251,35 @@ function renderizarListaEstudiantes() {
     const celdaApellido = document.createElement('td'); // Crea celda para el apellido
     celdaApellido.textContent = _estudiante.apellido;
     
+    const celdaDNI = document.createElement('td'); // Crea celda para el dni
+    celdaDNI.textContent = _estudiante.dni;
+
     const celdaEdad = document.createElement('td'); // Crea celda para la edad
     celdaEdad.textContent = _estudiante.edad;
 
     const celdaEmail = document.createElement('td'); // Crea celda para el email
     celdaEmail.textContent = _estudiante.email;
 
-    const celdaDNI = document.createElement('td'); // Crea celda para el email
-    celdaDNI.textContent = _estudiante.dni;
+    const celdaAccion = document.createElement('td'); // Crea celda para la acción de agregar materias
+    const botonAgregar = document.createElement('button');
+    botonAgregar.textContent = "Agregar Materias";
+    botonAgregar.className = 'btn btn-warning btn-sm';
+    botonAgregar.addEventListener('click', () => agregarMaterias(_estudiante)); // Añade evento de click al botón para agregar materias
+    
+    
+    celdaAccion.appendChild(botonAgregar); // Añade botón de agregar materias a la celda
+     
     fila.appendChild(celdaNumero); // Añade celda de número a la fila
     fila.appendChild(celdaNombre); // Añade celda de nombre a la fila
     fila.appendChild(celdaApellido); // Añade celda de apellido a la fila
+    fila.appendChild(celdaDNI); // Añade celda de dni
     fila.appendChild(celdaEdad); // Añade celda de edad a la fila
     fila.appendChild(celdaEmail); // Añade celda de email a la fila
-    fila.appendChild(celdaDNI); // Añade celda de dni
+    fila.appendChild(celdaAccion);
     listaElement.appendChild(fila); // Añade fila a la tabla
   });
 }
 
-function renderizarListaDeseados() {
-  const listaElement = document.getElementById('product-list');
-  listaElement.innerHTML = ''; // Limpia la lista antes de renderizar
-let i=1;
-productos.forEach((producto) => {
-  const fila = document.createElement('tr'); // Crea una nueva fila
-            
-  const celdaNumero = document.createElement('th'); // Crea celda para el número
-  celdaNumero.scope = "row";
-  celdaNumero.textContent = i++;
-  
-  const celdaNombre = document.createElement('td'); // Creacelda para el nombre del producto
-  celdaNombre.textContent = producto;
-  
-  fila.appendChild(celdaNumero); // Añade celda de número a la fila
-  fila.appendChild(celdaNombre); // Añade celda de nombre a la fila
-  
-  listaElement.appendChild(fila); // Añade fila a la tabla
-  });
-}
 
 function validarDNI(dni) { //Valida el dni que este entre cierto rango
   if (dni < 2000000 || dni > 70000000) {
@@ -301,6 +290,101 @@ function validarDNI(dni) { //Valida el dni que este entre cierto rango
   else 
   return true;
 }
+///
+///
+///
+///         PARTE 2
+///
+///
+class MateriaEstudiante {
+  constructor(estudiante, materia, nota) {
+    this.estudiante = estudiante;
+    this.materia = materia;
+    this.nota = nota;
+  }
+}
+
+let materiasEstudiantes = [];
+let materias = [];
+
+const materia1 = "Matemática";
+const materia2 = "Geografía";
+const materia3 = "Historia";
+const materia4 = "Literatura";
+
+materias.push(materia1, materia2, materia3, materia4);
+
+function agregarMaterias(estudiante) {
+  document.getElementById("agregarMaterias").style.display = "block";   
+  const estudiantesDiv = document.getElementById("estudiantes");
+  estudiantesDiv.style.display = "none";
+  estudiantesDiv.setAttribute("hidden", ""); // Agrega el atributo hidden
+  document.getElementById("h1Materias").innerHTML = "Agregar materias para " + estudiante.nombre + " " + estudiante.apellido;
+  
+  renderizarListaMaterias(estudiante); // Llamamos con el estudiante actual
+}
+
+function renderizarListaMaterias(estudiante) {
+  const listaElement = document.getElementById('materias-list');
+  listaElement.innerHTML = ''; // Limpia la lista antes de renderizar
+  let i = 1;
+
+  materias.forEach((materia) => {
+    const fila = document.createElement('tr'); // Crea una nueva fila
+
+    const celdaNumero = document.createElement('th'); // Crea celda para el número
+    celdaNumero.scope = "row";
+    celdaNumero.textContent = i++;
+
+    const celdaNombre = document.createElement('td'); // Crea celda para el nombre de la materia
+    celdaNombre.textContent = materia;
+
+    const celdaNota = document.createElement('td'); // Crea celda para la nota
+    const inputNota = document.createElement('input');
+    inputNota.type = "number";
+    inputNota.className="form-control";
+    inputNota.max = 10;
+    inputNota.min = 1;
+    inputNota.required = true;
+    celdaNota.appendChild(inputNota);
+
+    fila.appendChild(celdaNumero); // Añade celda de número a la fila
+    fila.appendChild(celdaNombre); // Añade celda de nombre a la fila
+    fila.appendChild(celdaNota); // Añade celda de nota a la fila
+
+    listaElement.appendChild(fila); // Añade fila a la tabla
 
 
+  });
+
+  // Añadimos un botón para guardar las notas
+  const guardarNotasBtn = document.createElement('button');
+  guardarNotasBtn.textContent = 'Guardar Notas';
+  guardarNotasBtn.addEventListener('click', () => guardarNotas(estudiante));
+  listaElement.appendChild(guardarNotasBtn);
+
+}
+
+function guardarNotas(estudiante) { //VER COMO GUARDAR ESTUDIANTE CON SUS MATERIAS Y NOTAS 
+  //VER DE HACERLO con un array EstudianteXMaterias donde se almacene otro array estudiante con las notas de cada materias
+  materias.forEach((materia) => {
+    const inputNota = document.getElementById(`nota-${materia}`);
+    const nota = inputNota ? inputNota.value : null;
+
+    if (nota) {
+      // Crear una nueva instancia de MateriaEstudiante con la nota y el estudiante actual
+      const materiaEstudiante = new MateriaEstudiante(estudiante, materia, nota);
+      materiasEstudiantes.push(materiaEstudiante);
+    }
+  });
  
+}
+
+
+
+function volverForm(){
+  document.querySelector(".form-horizontal").style.display="block";   
+  const estudiantesDiv = document.getElementById("estudiantes");
+  estudiantesDiv.style.display = "none";
+  estudiantesDiv.setAttribute("hidden", ""); // Agrega el atributo hidden
+}
